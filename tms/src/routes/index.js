d3d43e0f09b28b69a400292d44c4cfa89b64812e
rizasavinaa <rizasavinaa@gmail.com>
2025-03-06 19:25:19 +0700

    tampilan

--- tms/src/routes/index.js
+++ tms/src/routes/index.js
@@ -1,5 +1,19 @@
 const express   = require('express');
 const router = express.Router();
+const methodOverride = require('method-override');
+
+router.use(methodOverride('_method'));
+
+//load homepage
+router.get('/', (req,res)=>{
+    res.render('home');
+});
+
+//load menu user
+router.get('/user', (req,res)=>{
+    res.render('user');
+});
+
 const { allUsers, userForm, saveUser } = require('../controllers/UserController');
 
 router.get('/', allUsers);
